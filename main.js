afb130129286717c168958d66f596bf7e9e064a3
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2019-07-07 23:13:52 -0600

    js & css adds

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // Conver F --> C
 
 // Event Listeners
+document.getElementById('btn').addEventListener('click', main);
 
 // Functions
+function main() {
+    // INPUT - Get farenheit
+    let f = Number(document.getElementById('farenheit').value);
+
+    // PROCESS - use formula
+    let celsius = (f - 32) * (5 / 9);
+
+    // OUTPUT - display answer
+    document.getElementById('result').innerHTML = celsius.toFixed(2);
+}
